6fa9a666d511b97bec852faa79790c506007804a
Fábio Pereira <fabioegel@gmail.com>
2020-01-11 01:34:16 -0300

    Adding NANO node services to Fenix-IoT gateway

--- DPoW_FIOT_SERVER/Python/python_c_protocol/tests/test3.py
+++ DPoW_FIOT_SERVER/Python/python_c_protocol/tests/test3.py
@@ -8,10 +8,13 @@
 import paho.mqtt.client as mqtt
 import locale
 import fiot as fenixprotocol
+import requests
+import json
+import asyncio
 
-#Sáb 04 Jan 2020 22:32:53 -03
+#Qui 09 Jan 2020 22:40:39 -03 
 
-debug=True
+debug=False
 
 ################## ENCODING BEGIN ######################
 
@@ -74,10 +77,45 @@ def fenix_onreceive(protocol):
         publish_callback=None
     ret=None
     msg=""
+    err=0
+    errorname=""
+    parm=None
     if (command==fenixprotocol.CMD_GET_RAW_BALANCE):
-        ret=fenixiot.set_raw_balance(None, None, "5000000000000000000000000000")
-        if (ret==None):
-            msg="CMD_GET_RAW_BALANCE"
+        if (hasattr(protocol, "s7")):
+            parm='{"action":"account_balance","account":"'+protocol.s7.decode('ascii').rstrip('\0')+'"}'
+            loop=asyncio.get_event_loop()
+            try:
+                res=loop.run_until_complete(nano_node_srv(data=parm))
+            except Exception as e:
+                err=10000
+                errorname="Error: 'nano_node_srv' "+str(type(e))+" with message: "+str(e)+" Error no.: "+str(err)
+            if (res):
+                if (res.status_code==200):
+                    try:
+                        k=res.json()
+                        if ('balance' in k):
+                            balance=k['balance']
+                            print("Sucesso")
+                            ret=fenixiot.set_raw_balance(None, None, k['balance'])
+                            if (ret==None):
+                                msg="CMD_GET_RAW_BALANCE"
+                                err=fenixiot.getlasterror()
+                                errorname=fenixiot.geterrorname(err)
+                        elif ('error' in k):
+                            err=10003
+                            errorname="Error: NANO error "+k['error']+" Error no.: "+str(err)
+                        else:
+                            err=10004
+                            errorname="Error: NANO error unknown JSON param. Error no.: "+str(err)
+                    except Exception as e:
+                        err=10002
+                        errorname="Error: Can't parse NANO node JSON "+str(type(e))+" with message: "+str(e)+" Error no.: "+str(err)
+                else:
+                    err=10001
+                    errorname="Error: NANO node status code: "+str(res.status_code)
+        else:
+            err=10005
+            errorname="Error: Empty slot 7 fatal error "+str(err)
     elif (command==fenixprotocol.CMD_GET_FRONTIER):
         ret=fenixiot.set_frontier(None, None, "cb9850660e23e03205c2582875e9af3b2e8d075aeab5889de16bdc7cc76e5ef7")
         if (ret==None):
@@ -92,30 +130,31 @@ def fenix_onreceive(protocol):
                 print("Return (raw data to send): ->")
                 print(list(ret))
         else:
-            msg="get_dpow_hash_from_client()"
+                msg="get_dpow_hash_from_client()"
     elif (command==fenixprotocol.CMD_GET_REPRESENTATIVE):
         ret=fenixiot.send_representative(None, None, "xrb_1cb5fs7xmixqzpitfn9ouy4j1g3hjmdfudc1igt5xhwwps7qdku5htqxmznb")
         if (ret==None):
             msg="CMD_GET_REPRESENTATIVE"
-    else:
-        msg="UNKNOWN_COMMAND"
+        else:
+            msg="UNKNOWN_COMMAND"
+
     if (ret):
         client.publish(publish_callback, payload=ret, qos=2, retain=False)
         msg="Success"
     else:
         print("Data not sent to client '"+publish_callback+"' with request "+msg)
         msg="Fail"
-        err=fenixiot.getlasterror()
         print("Trying to send error reasons to client ...")
         if ((err==fenixprotocol.F_ERR_FORBIDDEN_OVFL_PUBL_STR)or(err==fenixprotocol.F_ERR_FORBIDDEN_NULL_PUB_STR)):
-            print("Could not send error to client. Invalid publish callback "+fenixprotocol.geterrorname(err))
+            print("Could not send error to client. Invalid publish callback "+errorname)
         else:
-            ret=fenixiot.senderrortoclient(None, err, "Error with name: "+fenixprotocol.geterrorname(err))
+            ret=fenixiot.senderrortoclient(None, err, "Error with name: "+errorname)
             if (ret):
                 client.publish(publish_callback, payload=ret, qos=2, retain=False)
             else:
                 err=fenixiot.getlasterror()
                 print("Sencond fail when sending error reason to client "+str(err)+" Error name: "+fenixprotocol.geterrorname(err))
+
     print("Finishing with "+msg)
 
 print("Initializing FIOT Python 3 library ...")
@@ -123,6 +162,13 @@ fenixiot=fenixprotocol.init(None)
 fenixiot.onerror(fenix_onerror)
 fenixiot.ondata(fenix_onreceive)
 print(fenixprotocol.about())
+
+################# GET PARAMETERS #######################
+
+NANO_NODE_URL="<YOUR_NANO_NODE_HERE>"
+
+async def nano_node_srv(data):
+   return requests.post(url=NANO_NODE_URL,data=data)
 
 ################### MQTT SERVICE BEGIN ##################
 
@@ -139,6 +185,5 @@ client.on_connect=on_connect
 client.on_message=on_message
 
 client.connect("localhost", 1883, 60)
-
 client.loop_forever()
 
